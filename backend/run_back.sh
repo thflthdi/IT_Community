9197254442d3079b31a30fb51ba5c5202f925b87
dokeey <ehdgnv@gmail.com>
2021-02-02 19:47:17 +0900

    ADD: Dockerfile 추가

--- backend/run_back.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-docker run \
-    -v $(pwd)/.:/backend \
-    -p 8080:8080 \
-    --rm -it \
-    ehdgnv/itc:1.0 
--- frontend/run_dev.sh
+++ frontend/run_dev.sh
@@ -3,6 +3,6 @@
 docker run \
    -p 80:3000 \
    -v $(pwd)/.:/frontend \
-   --rm -it -d \
-   thflthdi/reactsetting:1.0
+   --rm -it \
+   thflthdi/reactsetting:1.0 bash
 
