c70e36fdd7b205ba6197119c84ea84a0cf42392f
dokeey <ehdgnv@gmail.com>
2021-02-05 22:21:20 +0900

    ADD: Lib - API 문서 자동화 도구 drf-ysag 설치

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,28 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    validators=['flex', 'ssv'],
+    public=True,
+    permission_classes=(AllowAny,),
+)
 
 urlpatterns = [
+    path('swagger<str:format>', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
