673f014cca934381e995273622f64cdff30c07e5
thflthdi <thflthdi@gmail.com>
2021-02-06 22:39:20 +0900

    ADD: 로그인, 토큰 값 받아오기 구현

--- frontend/src/pages/auth.js
+++ frontend/src/pages/auth.js
@@ -4,7 +4,10 @@ import axios from 'axios';
 function Auth(){
     const [users,setUsers]=useState([]);
     const [userid,setUserid]=useState('');
+    const [loginid,setLoginid]=useState('');
     const [userpw,setUserpw]=useState('');
+    const [loginpw,setLoginpw]=useState('');
+    const [jwttoken,setJwttoken]=useState('');
 
     useEffect(()=>{
         const fetchUsers= async ()=>{
@@ -18,6 +21,7 @@ function Auth(){
         }
         fetchUsers();
     }, [])
+    //users가 바뀔 때 마다 useEffect 실행
 
     const joinUs = () => {
         console.log(userid)
@@ -33,6 +37,20 @@ function Auth(){
         joinUser();
     }
     
+    const login = () => {
+        const loginUser = async () => {
+            try{
+                const response = await axios.post('http://localhost:8080/accounts/token/',{username:loginid,password:loginpw});
+                console.log(response.data)
+                setJwttoken(response.data.token)
+                console.log(jwttoken)
+            }catch(e){
+                console.log(e)
+            }
+        }
+        loginUser();
+    }
+
     return(
     <ul>
         {users.map(user => (
@@ -42,9 +60,11 @@ function Auth(){
       ))}
       <li>id:<input name="userid" value={userid} onChange={(e)=>setUserid(e.target.value)}/></li>
       <li>PW:<input name="userpw" value={userpw} onChange={(e)=>setUserpw(e.target.value)}/></li>
-
       <li><button onClick={joinUs}>회원가입</button></li>
-      <li><button>로그인</button></li>
+
+      <li>id:<input name="loginid" value={loginid} onChange={(e)=>setLoginid(e.target.value)}/></li>
+      <li>PW:<input name="loginpw" value={loginpw} onChange={(e)=>setLoginpw(e.target.value)}/></li>
+      <li><button onClick={login}>로그인</button></li>
     </ul>
     )
 }
